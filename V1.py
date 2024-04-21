8826658ccfe66eae0eaf7e5812892f94ac54bb29
Vivek Chandra <158705881+VivekChandraa@users.noreply.github.com>
2024-04-21 22:57:16 +0530

    19-04-2024

--- V1.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from selenium import webdriver
-def test_log_vwo():
-    driver=webdriver.Chrome()
-    driver.get("https://app.vwo.com ")
-    driver.maximize_window()
-    print(driver.title)
-    assert driver.title=="Login - V"
